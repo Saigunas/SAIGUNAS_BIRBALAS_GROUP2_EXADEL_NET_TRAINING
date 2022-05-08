8a164642020205aac247dd683f72fe0f5a6887c4
Saigunas <saigunasb777@gmail.com>
2022-05-08 16:55:04 +0300

    Asks for input before printing

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -7,10 +7,12 @@ public class Program
     public static void Main(string[] args)
     {
         var a = 4;
+        var text = "Number: ";
 
         for (; a < 10; a++)
         {
-            Console.WriteLine(a);
+            Console.WriteLine(text + a);
+            Console.ReadKey();
         }
     }
 }
