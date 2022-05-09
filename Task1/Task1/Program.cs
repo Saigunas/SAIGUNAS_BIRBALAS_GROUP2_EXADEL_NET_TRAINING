8469fa13d4cc741c840f7bf5e11e3ace971576aa
Saigunas <saigunasb777@gmail.com>
2022-05-10 01:16:47 +0300

    Created task1 solution

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var a = 4;
+
+        for (; a < 10; a++)
+        {
+            Console.WriteLine(a);
+        }
+    }
+}
