aab3376d8b91170a738f5f2e7172dcae994d143a
Saigunas <saigunasb777@gmail.com>
2022-05-10 17:00:10 +0300

    Completed Task2

---
+++ Task2/Task2.1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+public class Program
+{
+
+    const int roundDecimal = 2;
+
+    public static void Main(string[] args)
+    {
+        int a, b;
+
+        while (true)
+        {
+            Console.Write("Input a: ");
+            a = int.Parse(Console.ReadLine());
+
+
+            if (a >= 0 && a <= 5)
+            {
+                break;
+            }
+            Console.WriteLine("Variable 'a' must be in the range 0 <= a <= 5");
+        }
+
+        while (true)
+        {
+            Console.Write("Input b: ");
+            b = int.Parse(Console.ReadLine());
+
+            if (b >= 0 && b <= 100)
+            {
+                break;
+            }
+            Console.WriteLine("Variable “b” must be in the range 0 <= b <= 100");
+        }
+
+
+        var program = new Program();
+
+        program.outputValues(program.getFactorial(a), program.getNaturalLog(b));
+    }
+
+    private double getFactorial(int a)
+    {
+        double factorial = 1;
+        for (int i = a; i > 0; i--)
+        {
+            factorial *= i;
+        }
+
+        factorial *= 0.05;
+        factorial = Math.Round(factorial, roundDecimal);
+
+        return factorial;
+    }
+
+    private double getNaturalLog(int b)
+    {
+        double naturalLog = Math.Log(b);
+        naturalLog = Math.Round(naturalLog, roundDecimal);
+
+        return naturalLog;
+    }
+
+    private void outputValues(double a, double b)
+    {
+
+        /*
+         * Numbers are already formatted to 2 decimals, but 'ToString' ensures that whole numbers are displayed with .00 (e.g. 1.00)
+         */
+
+        Console.Write($"\nNumber 'a' = {a.ToString("0.00")} and ");
+
+        if (a < b)
+        {
+            Console.Write("less than");
+        }
+        if (a > b)
+        {
+            Console.Write("more than");
+        }
+        if (a == b)
+        {
+            Console.Write("equals");
+        }
+
+        Console.Write($" number 'b' = {b.ToString("0.00")}\n");
+    }
+}
---
+++ Task2/Task2.2/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+public class Program
+{
+
+    const int maxFileLength = 300;
+
+    public static void Main(string[] args)
+    {
+        string file1Contents = System.IO.File.ReadAllText("..\\..\\..\\file1.txt");
+        string file2Contents = System.IO.File.ReadAllText("..\\..\\..\\file2.txt");
+        string file3Contents = System.IO.File.ReadAllText("..\\..\\..\\file3.txt");
+
+        List<string> fileList = new List<string>() {file1Contents, file2Contents, file3Contents};
+        List<string> changedFileList = new List<string>();
+
+        var program = new Program();
+        changedFileList = program.modifyList(fileList);
+
+        string path = "..\\..\\..\\output.txt";
+        File.WriteAllText(path, String.Empty);
+        File.AppendAllLines(path, changedFileList);
+
+        File.Delete("..\\..\\..\\file1.txt");
+        File.Delete("..\\..\\..\\file2.txt");
+        File.Delete("..\\..\\..\\file3.txt");
+
+    }
+
+
+    List<string> modifyList(List<string> fileList)
+    {
+        List<string> changedFileList = new List<string>();
+
+        for (int i = 0; i < fileList.Count; i++)
+        {
+            string itemToChange = fileList[i];
+
+            //Everything is applied separately for the sake of readability and easier modification.
+            itemToChange = applyLengthLimit(itemToChange);
+            itemToChange = reverseString(itemToChange);
+            itemToChange = sortWordByUpperCase(itemToChange);
+            itemToChange = filterString(itemToChange);
+
+
+            changedFileList.Add(itemToChange);
+        }
+
+        changedFileList = changedFileList.OrderBy(x => x.Length).ToList();
+
+        return changedFileList;
+    }
+
+    string filterString(string toFilter)
+    {
+        char[] characters = toFilter.ToArray();
+
+        characters = Array.FindAll(characters, c => {
+            return (char.IsWhiteSpace(c) || char.IsLetter(c));
+        });
+
+        string filteredString = new string(characters);
+
+        return filteredString;
+    }
+
+    string sortWordByUpperCase(string toSort)
+    {
+
+        List<string> words = new List<string>();
+        words.AddRange(toSort.Split(' '));
+
+        for(int i = 0; i < words.Count; i++)
+        {
+            List<char> chars = new List<char>();
+            chars.AddRange(words[i]);
+
+            var sortedCharacters = chars.OrderBy(c => !char.IsUpper(c)).ToArray();
+            words[i] = new string(sortedCharacters);
+        }
+
+        string sortedString = string.Join(" ", words);
+        return sortedString;
+    }
+
+    string reverseString(string toReverse)
+    {
+        char[] characters = toReverse.ToArray();
+        
+        Array.Reverse(characters);
+        string reversedString = new string(characters);
+
+        return reversedString;
+    }
+
+    string applyLengthLimit(string toLimit)
+    {
+        if (toLimit.Length > 300)
+        {
+            toLimit = toLimit.Substring(0, maxFileLength);
+        }
+        return toLimit;
+    }
+
+
+}
