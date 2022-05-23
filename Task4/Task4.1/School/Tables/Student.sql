5c57b1a7834ff5d18c5ee1cdcd85308c576a23ad
Saigunas <saigunasb777@gmail.com>
2022-05-23 15:52:00 +0300

    Readded Task4

---
+++ Task4/Task4.1/School/Tables/Student.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Student] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY (1, 1), 
+    [ClassId]     INT   NOT NULL,
+    [FirstName]   NVARCHAR (50)  NOT NULL,
+    [LastName]    NVARCHAR (50)  NOT NULL,
+    [PhoneNumber] INT            NOT NULL,
+    [Address]     NVARCHAR (50) NOT NULL,
+	CONSTRAINT [FK_Student_Class] FOREIGN KEY ([ClassId]) REFERENCES [dbo].[Class] ([Id]) ON DELETE CASCADE
+);
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_Student_LastName]
+    ON [dbo].[Student]([LastName] ASC);
+
---
+++ Task4/Task4.1/School/Views/FullStudents.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW [dbo].[FullStudents]
+	AS 
+	SELECT Student.FirstName, Student.LastName, Student.PhoneNumber, Student.Address, CONCAT(Class.Number, Class.Letter) AS 'Class'
+	FROM Student 
+		JOIN Class ON Class.Id = Student.ClassID
+	;
