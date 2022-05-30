17afff436a8e7c0f717964d7a1dde1defa83adc0
Saigunas <saigunasb777@gmail.com>
2022-05-30 12:32:08 +0300

    Set basic structure for Task5 code

---
+++ Task5/Task5/DataAccessLayer/Core/Repositories/ISubjectRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task5.DataAccessLayer.Core.Domain;
+
+namespace Task5.DataAccessLayer.Repositories
+{
+    public interface ISubjectRepository : IRepository<Subject>
+    {
+        void CreateSubject();
+
+        void UpdateSubject();
+
+        void DeleteSubject();
+
+        void ReadSubject();
+    }
+}
---
+++ Task7/Task7/DataAccessLayer/Persistence/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task5.DataAccessLayer.Persistence.Repositories;
+using Task5.DataAccessLayer.Repositories;
+
+namespace Task5.DataAccessLayer.Persistence
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly SchoolContext _context;
+
+        public UnitOfWork(SchoolContext context)
+        {
+            _context = context;
+            Subjects = new SubjectRepository(_context);
+        }
+
+        public ISubjectRepository Subjects { get; private set; }
+
+        public int Save()
+        {
+            return _context.SaveChanges();
+        }
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
